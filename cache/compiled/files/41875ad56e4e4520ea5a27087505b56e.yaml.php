6734b98f919ca915f1068e9f9a8f8c6677da788e
Janes Cleston <cleston.silva@rsb.org.br>
2025-08-29 15:14:30 -0300

    cache

---
+++ cache/compiled/files/41875ad56e4e4520ea5a27087505b56e.yaml.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'C:/Users/cleston.silva/Desktop/Dev/grav2/grav/user/config/security.yaml',
+    'modified' => 1756490374,
+    'size' => 21,
+    'data' => [
+        'salt' => '4981veG2sOtUhg'
+    ]
+];
